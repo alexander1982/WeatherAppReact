9ca96a6117ca25dab6b30917c4c3c54c8b777980
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-05-02 17:33:19 +0300

    Addeed to server

--- server/Server.js
+++ server/Server.js
@@ -4,11 +4,20 @@ let bodyParser = require('body-parser');
 let path = require('path');
 
 let publicPath = path.join(__dirname, '../public');
-let port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+	if(req.headers['x-forward-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+	
+});
 
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
